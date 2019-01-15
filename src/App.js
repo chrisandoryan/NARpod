ae14c72be15457cf16c059baadd1c97d799df835
Ando <chrisando.siahaan@gmail.com>
2019-01-15 22:27:24 +0700

    added timestamp hotkey

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dropzone from 'react-dropzone';
 import Modal from 'react-responsive-modal';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ContentEditable from 'react-contenteditable'
+import { HotKeys } from 'react-hotkeys';
 
 // Be sure to include styles at some point, probably during your bootstraping
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
@@ -13,21 +14,59 @@ import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import 'brace/mode/java';
 import 'brace/theme/github';
 
+class Note extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      toggleClass: true,
+      modalOpen: false,
+      files: [],
+      selectedFile: {},
+      displayContent: '',
+      selectedFilePosition: -1,
+    }
+  }
+
+  addTimestamp = () => {
+    let editor = this.refs.ace.editor;
+    console.log(editor.getCursorPosition())
+    editor.session.insert({row: editor.getCursorPosition().row, column: 0}, new Date().toLocaleTimeString() + '\n');
+  }
+
+  render() {
+    const handlers = {
+      ADD_TIMESTAMP: this.addTimestamp
+    };
+    return (
+      <HotKeys handlers={handlers}>
+        <AceEditor
+          ref="ace"
+          mode="java"
+          theme="github"
+          value={this.state.displayContent}
+          onChange={this.onChange}
+          name="editor"
+          height={window.innerHeight}
+          fontSize={18}
+          editorProps={{ $blockScrolling: Infinity }}
+        />
+      </HotKeys >
+    )
+  }
+}
+
 class App extends Component {
 
-  note = () => {
-    return (
-      <AceEditor
-        mode="java"
-        theme="github"
-        value={this.state.displayContent}
-        onChange={this.onChange}
-        name="editor"
-        height={window.innerHeight}
-        fontSize={18}
-        editorProps={{ $blockScrolling: Infinity }}
-      />
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleClass: true,
+      modalOpen: false,
+      files: [],
+      selectedFile: {},
+      displayContent: '',
+      selectedFilePosition: -1,
+    }
   }
 
   handleChange = e => {
@@ -86,73 +125,65 @@ class App extends Component {
     )
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      toggleClass: true,
-      modalOpen: false,
-      files: [],
-      selectedFile: {},
-      displayContent: '',
-      selectedFilePosition: -1,
-    }
-    this.contentEditable = React.createRef();
-  }
-
   render() {
+    const keyMap = {
+      ADD_TIMESTAMP: 'alt+w',
+    };
     return (
       <React.Fragment>
-        <div className={`container-fluid ${this.state.toggleClass ? 'toggled' : ''}`}>
-          <div className="sidebar">
-            <SideNav
-              onSelect={(selected) => {
-                // Add your code here
+        <HotKeys keyMap={keyMap}>
+          <div className={`container-fluid ${this.state.toggleClass ? 'toggled' : ''}`}>
+            <div className="sidebar">
+              <SideNav
+                onSelect={(selected) => {
+                  // Add your code here
 
-              }}
-            >
-              <SideNav.Toggle
-                onClick={() => {
-                  this.setState({ toggleClass: !this.state.toggleClass })
                 }}
-              />
-              <SideNav.Nav defaultSelected="">
-                <NavItem eventKey="upload" onClick={this.onOpenModal}>
-                  <NavIcon>
-                    <i className="fa fa-sticky-note " style={{ fontSize: '1.75em' }} />
-                  </NavIcon>
-                  <NavText>
-                    Note
+              >
+                <SideNav.Toggle
+                  onClick={() => {
+                    this.setState({ toggleClass: !this.state.toggleClass })
+                  }}
+                />
+                <SideNav.Nav defaultSelected="">
+                  <NavItem eventKey="upload" onClick={this.onOpenModal}>
+                    <NavIcon>
+                      <i className="fa fa-sticky-note " style={{ fontSize: '1.75em' }} />
+                    </NavIcon>
+                    <NavText>
+                      Note
                 </NavText>
-                </NavItem>
-                <NavItem eventKey="files">
-                  <NavIcon>
-                    <i className="fa fa-desktop" style={{ fontSize: '1.75em' }} />
-                  </NavIcon>
-                  <NavText>
-                    Bench Mapping
+                  </NavItem>
+                  <NavItem eventKey="files">
+                    <NavIcon>
+                      <i className="fa fa-desktop" style={{ fontSize: '1.75em' }} />
+                    </NavIcon>
+                    <NavText>
+                      Bench Mapping
                 </NavText>
-                  {
-                    this.state.files.map((file, i) => (
-                      <NavItem eventKey="files/viewfile" key={i} onClick={() => { this.displayContent(i) }}>
-                        <NavText>
-                          {file.fileName}
-                        </NavText>
-                      </NavItem>
-                    ))
-                  }
-                </NavItem>
-              </SideNav.Nav>
-            </SideNav>
+                    {
+                      this.state.files.map((file, i) => (
+                        <NavItem eventKey="files/viewfile" key={i} onClick={() => { this.displayContent(i) }}>
+                          <NavText>
+                            {file.fileName}
+                          </NavText>
+                        </NavItem>
+                      ))
+                    }
+                  </NavItem>
+                </SideNav.Nav>
+              </SideNav>
+            </div>
+            <div className="content">
+              <Router>
+                <Fragment>
+                  <Route exact path="/" component={Note} />
+                  <Route exact path="/mapping" component={this.mapping} />
+                </Fragment>
+              </Router>
+            </div>
           </div>
-          <div className="content">
-            <Router>
-              <Fragment>
-                <Route exact path="/" component={this.note} />
-                <Route exact path="/mapping" component={this.mapping} />
-              </Fragment>
-            </Router>
-          </div>
-        </div>
+        </HotKeys>
       </React.Fragment>
     );
   }
